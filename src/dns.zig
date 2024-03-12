211f55d4787e586d33def445d84da4211f24d43c
Diogo Silva <diogo@diogok.net>
2024-03-13 00:26:40 +0900

    get nameservers fron config, more refactoring

--- src/dns.zig
+++ src/dns.zig
@@ -4,6 +4,44 @@ const os = std.os;
 
 const log = std.log.scoped(.with_dns);
 
+pub fn resolve(allocator: std.mem.Allocator, name: []const u8, resource_type: ResourceType) !Reply {
+    const sock = try os.socket(os.AF.INET, os.SOCK.DGRAM | os.SOCK.CLOEXEC, 0);
+    defer os.close(sock);
+
+    // TODO: enabe override of dns server
+    const servers = try get_nameservers(allocator);
+    defer allocator.free(servers);
+    for (servers) |address| {
+        try os.connect(sock, &address.any, address.getOsSockLen());
+        log.info("Connected to {any}", .{address});
+
+        // TODO: settimeout
+        {
+            var buffer: [512]u8 = undefined;
+            var stream = std.io.fixedBufferStream(&buffer);
+            const writer = stream.writer();
+
+            try writeQuery(writer, name, resource_type);
+            const req = stream.getWritten();
+            _ = try os.send(sock, req[0..28], 0);
+        }
+
+        {
+            var buffer: [512]u8 = undefined;
+            _ = try os.recv(sock, &buffer, 0);
+            var stream = std.io.fixedBufferStream(&buffer);
+            const reader = stream.reader();
+
+            const reply = try readReply(allocator, reader);
+            if (reply.records.len > 0) {
+                return reply;
+            }
+        }
+    }
+
+    return error.UnableToResolve;
+}
+
 pub fn writeQuery(writer: anytype, name: []const u8, resource_type: ResourceType) !void {
     // build header with flags
     const flags = Flags{
@@ -12,17 +50,16 @@ pub fn writeQuery(writer: anytype, name: []const u8, resource_type: ResourceType
         .recursion_desired = true,
         .recursion_available = true,
     };
-    log.info("Query flags: {any}", .{flags});
     const header = Header{
         .ID = mkid(),
-        .flags = @bitCast(flags),
+        .flags = flags,
         .number_of_questions = 1,
     };
     log.info("Query header: {any}", .{header});
 
     // write header
     try writer.writeInt(u16, header.ID, .big);
-    try writer.writeInt(u16, header.flags, .big);
+    try writer.writeInt(u16, @bitCast(header.flags), .big);
     try writer.writeInt(u16, header.number_of_questions, .big);
     try writer.writeInt(u16, header.number_of_answers, .big);
     try writer.writeInt(u16, header.number_of_authority_resource_records, .big);
@@ -90,32 +127,30 @@ test "Write a name" {
     try testing.expectEqualStrings("\x03www\x07example\x03com\x00", written);
 }
 
+// To read the u16 as big endian
 const Pack = std.PackedIntSliceEndian(u16, .big);
+const Pack2 = std.PackedIntSliceEndian(u32, .big);
 
-pub fn read_reply(allocator: std.mem.Allocator, reader: anytype) !Reply {
-    // read all of the request, because we might need the bytes for pointer labels
+pub fn readReply(allocator: std.mem.Allocator, reader: anytype) !Reply {
+    // read all of the request, because we might need the all bytes for pointer/compressed labels
     var pos: usize = 0;
     var buffer: [512]u8 = undefined;
     _ = try reader.read(&buffer);
 
-    const header_pack = Pack.init(buffer[0..12], 6);
+    const header_pack = Pack.init(buffer[0..12], 6); // To read the u16 as big endian
     const header = Header{
         .ID = header_pack.get(0),
-        .flags = header_pack.get(1),
+        .flags = @bitCast(header_pack.get(1)),
         .number_of_questions = header_pack.get(2),
         .number_of_answers = header_pack.get(3),
         .number_of_authority_resource_records = header_pack.get(4),
         .number_of_additional_resource_records = header_pack.get(5),
     };
     log.info("Reply header: {any}", .{header});
-    const flags: Flags = @bitCast(header.flags);
-    log.info("Reply flags: {any}", .{flags});
-
     pos += 12;
 
-    var questions = std.ArrayList(Question).init(allocator);
-
     // read questions
+    var questions = std.ArrayList(Question).init(allocator);
     var i: usize = 0;
     while (i < header.number_of_questions) : (i += 1) {
         const name_len = getNameLen(buffer[pos..]);
@@ -137,7 +172,6 @@ pub fn read_reply(allocator: std.mem.Allocator, reader: anytype) !Reply {
     }
 
     var records = std.ArrayList(Record).init(allocator);
-
     i = 0;
     while (i < header.number_of_answers) : (i += 1) {
         const name_len = getNameLen(buffer[pos..]);
@@ -148,8 +182,11 @@ pub fn read_reply(allocator: std.mem.Allocator, reader: anytype) !Reply {
         const r_type = q_pack.get(0);
         const r_class = q_pack.get(1);
         pos += 4;
-        // TODO: read ttl : u32
+
+        const ttl_pack = Pack2.init(buffer[pos .. pos + 4], 1);
+        const ttl = ttl_pack.get(0);
         pos += 4;
+
         const l_pack = Pack.init(buffer[pos .. pos + 4], 1);
         const data_len = l_pack.get(0);
         pos += 2;
@@ -161,7 +198,7 @@ pub fn read_reply(allocator: std.mem.Allocator, reader: anytype) !Reply {
         const record = Record{
             .resource_type = @enumFromInt(r_type),
             .resource_class = @enumFromInt(r_class),
-            .ttl = 0,
+            .ttl = ttl,
             .name = name,
             .data = data,
         };
@@ -172,7 +209,6 @@ pub fn read_reply(allocator: std.mem.Allocator, reader: anytype) !Reply {
     return Reply{
         .allocator = allocator,
         .header = header,
-        .flags = flags,
         .questions = try questions.toOwnedSlice(),
         .records = try records.toOwnedSlice(),
     };
@@ -201,11 +237,11 @@ test "Read reply" {
     var stream = std.io.fixedBufferStream(&buffer);
     const reader = stream.reader();
 
-    const reply = try read_reply(testing.allocator, reader);
+    const reply = try readReply(testing.allocator, reader);
     defer reply.deinit();
 
     try testing.expectEqual(reply.header.ID, 256);
-    try testing.expectEqual(reply.flags.recursion_desired, true);
+    try testing.expectEqual(reply.header.flags.recursion_desired, true);
     try testing.expectEqual(reply.header.number_of_questions, 1);
     try testing.expectEqual(reply.header.number_of_answers, 1);
     try testing.expectEqual(reply.questions.len, 1);
@@ -218,7 +254,7 @@ test "Read reply" {
     try testing.expectEqualStrings(reply.records[0].name, "example.com");
     try testing.expectEqual(reply.records[0].resource_type, ResourceType.A);
     try testing.expectEqual(reply.records[0].resource_class, ResourceClass.IN);
-    //try testing.expectEqual(reply.records[0].ttl, 16777472); // TODO: read ttl
+    try testing.expectEqual(reply.records[0].ttl, 16777472); // TODO: read ttl
     try testing.expectEqual(reply.records[0].data.len, 4);
 
     try testing.expectEqual(reply.records[0].data[0], 1);
@@ -228,23 +264,24 @@ test "Read reply" {
 }
 
 fn getNameLen(buffer: []u8) usize {
-    var skip: usize = 0;
-
+    var skip: usize = 1;
     var len = buffer[0];
-    skip += 1;
-    while (len > 0) {
+    while (len > 0) : (skip += 1) {
         if (len == 192) {
-            skip += 1;
-            break;
+            // this is a pointer to another memory region
+            // used for compressed labels
+            return skip + 1;
         } else {
             skip += len;
             len = buffer[skip];
-            skip += 1;
         }
     }
-
     return skip;
 }
+
+test "Get name len" {}
+
+test "Get name len with pointer" {}
 
 fn readName(allocator: std.mem.Allocator, full_buffer: []u8, buffer: []u8) ![]const u8 {
     var arr = std.ArrayList(u8).init(allocator);
@@ -275,6 +312,19 @@ fn readName(allocator: std.mem.Allocator, full_buffer: []u8, buffer: []u8) ![]co
     return arr.toOwnedSlice();
 }
 
+test "Read name len" {}
+
+test "Read name len with pointer" {}
+
+const Header = packed struct {
+    ID: u16,
+    flags: Flags,
+    number_of_questions: u16 = 0,
+    number_of_answers: u16 = 0,
+    number_of_authority_resource_records: u16 = 0,
+    number_of_additional_resource_records: u16 = 0,
+};
+
 const Flags = packed struct {
     query_or_reply: QueryOrReply = .query,
     opcode: Opcode = .query,
@@ -284,15 +334,6 @@ const Flags = packed struct {
     recursion_available: bool = false,
     zero: u3 = 0,
     response_code: ReplyCode = .no_error,
-};
-
-const Header = packed struct {
-    ID: u16,
-    flags: u16 = 0,
-    number_of_questions: u16 = 0,
-    number_of_answers: u16 = 0,
-    number_of_authority_resource_records: u16 = 0,
-    number_of_additional_resource_records: u16 = 0,
 };
 
 const QueryOrReply = enum(u1) { query, reply };
@@ -350,7 +391,6 @@ const Reply = struct {
     allocator: std.mem.Allocator,
 
     header: Header,
-    flags: Flags,
     questions: []Question,
     records: []Record,
 
@@ -368,10 +408,55 @@ const Reply = struct {
 };
 
 fn mkid() u16 {
-    var ts: os.timespec = undefined;
-    os.clock_gettime(os.CLOCK.REALTIME, &ts) catch {};
-    const UInt = std.meta.Int(.unsigned, @bitSizeOf(@TypeOf(ts.tv_nsec)));
-    const unsec: UInt = @bitCast(ts.tv_nsec);
-    const id: u32 = @truncate(unsec + unsec / 65536);
-    return @truncate(id);
+    var rnd = std.Random.DefaultPrng.init(0);
+    return rnd.random().int(u16);
+}
+
+pub fn get_nameservers(allocator: std.mem.Allocator) ![]std.net.Address {
+    const resolvconf = try std.fs.openFileAbsolute("/etc/resolv.conf", .{});
+    defer resolvconf.close();
+    const reader = resolvconf.reader();
+    return parse_resolvconf(allocator, reader);
+}
+
+fn parse_resolvconf(allocator: std.mem.Allocator, reader: anytype) ![]std.net.Address {
+    var addresses = std.ArrayList(std.net.Address).init(allocator);
+
+    var buffer: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        if (line.len > 10 and std.mem.eql(u8, line[0..10], "nameserver")) {
+            var pos: usize = 10;
+            while (pos < line.len and std.ascii.isWhitespace(line[pos])) : (pos += 1) {}
+            const start: usize = pos;
+            while (pos < line.len and (std.ascii.isHex(line[pos]) or line[pos] == '.' or line[pos] == ':')) : (pos += 1) {}
+            const address = std.net.Address.resolveIp(line[start..pos], 53) catch continue;
+            try addresses.append(address);
+        }
+    }
+
+    return try addresses.toOwnedSlice();
+}
+
+test "nameservers" {
+    const resolvconf =
+        \\;a comment
+        \\# another comment
+        \\
+        \\domain    example.com
+        \\
+        \\nameserver     127.0.0.53 # comment after
+        \\nameserver ::ff
+    ;
+    var stream = std.io.fixedBufferStream(resolvconf);
+    const reader = stream.reader();
+
+    const addresses = try parse_resolvconf(testing.allocator, reader);
+    defer testing.allocator.free(addresses);
+
+    const ip4 = try std.net.Address.parseIp4("127.0.0.53", 53);
+    const ip6 = try std.net.Address.parseIp6("::ff", 53);
+
+    try testing.expectEqual(2, addresses.len);
+    try testing.expect(addresses[0].eql(ip4));
+    try testing.expect(addresses[1].eql(ip6));
 }
--- src/main.zig
+++ src/main.zig
@@ -10,46 +10,7 @@ pub fn main() !void {
     defer std.debug.assert(gpa.deinit() != .leak);
     const allocator = gpa.allocator();
 
-    const sock = try os.socket(os.AF.INET, os.SOCK.DGRAM | os.SOCK.CLOEXEC, 0);
-    errdefer os.close(sock);
-
-    //const addr = try std.net.Address.resolveIp("224.0.0.251", 5353);
-    const addr = try std.net.Address.resolveIp("127.0.0.53", 53);
-    try os.connect(sock, &addr.any, addr.getOsSockLen());
-    log.info("Connected at least", .{});
-
-    {
-        var buffer: [512]u8 = undefined;
-        var stream = std.io.fixedBufferStream(&buffer);
-        const writer = stream.writer();
-
-        try dns.writeQuery(writer, "google.com", .A);
-        const req = stream.getWritten();
-        log.info("Request, kinda: ({d}) {b}", .{ req.len, req });
-        _ = try os.send(sock, req[0..28], 0);
-    }
-
-    log.info("Sent request", .{});
-    std.time.sleep(3 * 1000 * 1000 * 1000);
-    log.info("Lets see if there is answer", .{});
-
-    {
-        var buffer: [512]u8 = undefined;
-        var stream = std.io.fixedBufferStream(&buffer);
-        var writer = stream.writer();
-        const reader = stream.reader();
-
-        var buffer0: [512]u8 = undefined;
-        const recvd = try os.recv(sock, &buffer0, 0);
-        const written = try writer.write(buffer0[0..recvd]);
-        log.info("Reply, kinda: ({d}) {b}", .{ written, buffer0[0..written] });
-
-        stream.reset();
-
-        const reply = try dns.read_reply(allocator, reader);
-        defer reply.deinit();
-        log.info("Reply: {any}", .{reply});
-    }
-
-    os.close(sock);
+    const reply = try dns.resolve(allocator, "diogok.net", .A);
+    defer reply.deinit();
+    log.info("Reply: {any}", .{reply.records[0]});
 }
