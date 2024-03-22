3d8bffc05bd8443933776d2550e027b3d814f89b
Diogo Silva <diogo@diogok.net>
2024-03-22 18:29:16 +0900

    rm unused imports

--- src/dns/dns_sd.zig
+++ src/dns/dns_sd.zig
@@ -1,8 +1,4 @@
 const std = @import("std");
-
-const io = @import("io.zig");
-const data = @import("data.zig");
-const udp = @import("udp.zig");
 const core = @import("core.zig");
 
 const log = std.log.scoped(.with_dns);
